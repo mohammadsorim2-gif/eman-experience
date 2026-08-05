455e195656fc9fcad3274bdecdc5de0aba750c51
mohammadsorim2-gif <mohammad.sorim2@gmail.com>
2026-08-05 21:58:43 +0300

    Initialize Firebase from generated platform options

--- lib/core/firebase/firebase_runtime.dart
+++ lib/core/firebase/firebase_runtime.dart
@@ -1,39 +1,27 @@
 import 'package:firebase_core/firebase_core.dart';
 
+import '../../firebase_options.dart';
+
 class FirebaseRuntime {
   FirebaseRuntime._();
 
   static bool _initialized = false;
   static String? _error;
 
-  static bool get initialized => _initialized;
+  static bool get initialized => _initialized || Firebase.apps.isNotEmpty;
   static String? get error => _error;
-
-  static bool get configured {
-    const apiKey = String.fromEnvironment('FIREBASE_API_KEY');
-    const appId = String.fromEnvironment('FIREBASE_APP_ID');
-    const projectId = String.fromEnvironment('FIREBASE_PROJECT_ID');
-    return apiKey.isNotEmpty && appId.isNotEmpty && projectId.isNotEmpty;
-  }
+  static bool get configured => true;
 
   static Future<bool> initialize() async {
-    if (_initialized) return true;
-    if (!configured) {
-      _error = 'Firebase environment values are missing.';
-      return false;
+    if (initialized) {
+      _initialized = true;
+      return true;
     }
 
     try {
-      const options = FirebaseOptions(
-        apiKey: String.fromEnvironment('FIREBASE_API_KEY'),
-        appId: String.fromEnvironment('FIREBASE_APP_ID'),
-        messagingSenderId: String.fromEnvironment('FIREBASE_MESSAGING_SENDER_ID'),
-        projectId: String.fromEnvironment('FIREBASE_PROJECT_ID'),
-        authDomain: String.fromEnvironment('FIREBASE_AUTH_DOMAIN'),
-        storageBucket: String.fromEnvironment('FIREBASE_STORAGE_BUCKET'),
-        measurementId: String.fromEnvironment('FIREBASE_MEASUREMENT_ID'),
+      await Firebase.initializeApp(
+        options: DefaultFirebaseOptions.currentPlatform,
       );
-      await Firebase.initializeApp(options: options);
       _initialized = true;
       _error = null;
       return true;
